1d1fbda703e01621c18a2e77a0d3c58cdb0602f1
SerVac <vacsaman@gmail.com>
2017-08-22 19:10:01 +0300

    missed entity added

---
+++ src/main/java/ru/office/data/entity/Department.java
@@ -0,0 +1,59 @@
+package ru.office.data.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "department")
+public class Department implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	/*
+	id
+	office_id
+	sub_department_id
+	chief_id
+	worker_id
+	title
+	address
+	 */
+	@Id
+	@SequenceGenerator(name="department_generator", sequenceName="department_sequence")
+	@GeneratedValue(generator = "department_generator")
+	private Long id;
+
+	@Column(nullable = false)
+	private String title;
+
+	/*@ManyToOne(cascade={CascadeType.ALL})
+	@JoinColumn(name="sub_department_id")
+	private Department subDepartment;
+
+	@OneToMany(mappedBy="sub_department")
+	private Set<Department> subDepartments = new HashSet<Department>();*/
+
+//	@ManyToOne(cascade = CascadeType.ALL)
+
+
+	protected Department() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
